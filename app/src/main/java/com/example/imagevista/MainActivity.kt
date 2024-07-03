fd4601579cdd14c91257b82b37aafe5279772005
Bhargav200004 <Bhargav200004@gmail.com>
2024-07-03 15:10:17 +0530

    adding top bar and some modification

--- app/src/main/java/com/example/imagevista/MainActivity.kt
+++ app/src/main/java/com/example/imagevista/MainActivity.kt
@@ -1,25 +1,43 @@
 package com.example.imagevista
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.imagevista.ui.homeScreen.HomeScreen
 import com.example.imagevista.ui.homeScreen.HomeScreenViewModel
 import com.example.imagevista.ui.theme.ImageVistaTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ImageVistaTheme {
+                val scrollBehaviour = TopAppBarDefaults.enterAlwaysScrollBehavior()
                 val viewModel = viewModel<HomeScreenViewModel>()
-                HomeScreen(
-                    image = viewModel.image,
-                    onImageClick = {}
-                )
+                Scaffold (
+                    modifier = Modifier.fillMaxSize()
+                        .nestedScroll(scrollBehaviour.nestedScrollConnection)
+                ){
+                    HomeScreen(
+                        scrollBehaviour = scrollBehaviour,
+                        image = viewModel.image,
+                        onImageClick = {},
+                        onSearchClick = {},
+                        onFABClick = {}
+                    )
+                }
             }
         }
     }
--- app/src/main/java/com/example/imagevista/ui/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/imagevista/ui/homeScreen/HomeScreen.kt
@@ -1,29 +1,59 @@
 package com.example.imagevista.ui.homeScreen
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.room.util.TableInfo
-import com.example.imagevista.data.remote.dto.UnsplashImageDto
+import com.example.imagevista.R
 import com.example.imagevista.domain.model.UnsplashImage
-import com.example.imagevista.ui.component.ImageCard
 import com.example.imagevista.ui.component.ImageVerticalGrid
+import com.example.imagevista.ui.component.ImageVistaTopApp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
-    image : List<UnsplashImage>,
-    onImageClick : (String) -> Unit
+    image: List<UnsplashImage>,
+    onImageClick: (String) -> Unit,
+    onSearchClick: () -> Unit,
+    scrollBehaviour: TopAppBarScrollBehavior,
+    onFABClick : () -> Unit
 ) {
-   ImageVerticalGrid(
-       images = image,
-       onImageClickable = onImageClick
-   )
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ImageVistaTopApp(
+                scrollBehaviour = scrollBehaviour,
+                onSearchClick = onSearchClick
+            )
+            ImageVerticalGrid(
+                images = image,
+                onImageClickable = onImageClick
+            )
+        }
+        FloatingActionButton(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(24.dp),
+            onClick = onFABClick
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.icon_save),
+                contentDescription = "Save button" ,
+                tint = MaterialTheme.colorScheme.onBackground
+            )
+        }
+    }
+
 }
