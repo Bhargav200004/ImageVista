5912a5ff7bd1a7d24d39775e602d8fecbba1451a
Bhargav200004 <Bhargav200004@gmail.com>
2024-07-07 18:57:46 +0530

    implementation of network manager

--- app/src/main/java/com/example/imagevista/MainActivity.kt
+++ app/src/main/java/com/example/imagevista/MainActivity.kt
@@ -8,17 +8,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
+import com.example.imagevista.domain.model.NetworkStatus
+import com.example.imagevista.domain.repository.NetworkConnectivityObserver
 import com.example.imagevista.ui.navigation.NavGraphSetup
 import com.example.imagevista.ui.theme.ImageVistaTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var connectivityObserver: NetworkConnectivityObserver
+
     @OptIn(ExperimentalMaterial3Api::class)
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +36,9 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         enableEdgeToEdge()
         setContent {
+
+            val status by connectivityObserver.networkStatus.collectAsState()
+
             ImageVistaTheme {
                 val navController = rememberNavController()
                 val scrollBehaviour = TopAppBarDefaults.enterAlwaysScrollBehavior()
@@ -33,7 +46,15 @@ class MainActivity : ComponentActivity() {
                 Scaffold (
                     modifier = Modifier
                         .fillMaxSize()
-                        .nestedScroll(scrollBehaviour.nestedScrollConnection)
+                        .nestedScroll(scrollBehaviour.nestedScrollConnection),
+                    bottomBar = {
+                        if (status == NetworkStatus.Connected){
+                            Text(text = "Connected to Internet")
+                        }
+                        else{
+                            Text(text = "No Internet Connection")
+                        }
+                    }
                 ){
                     NavGraphSetup(
                         navController = navController,
---
+++ app/src/main/java/com/example/imagevista/domain/repository/NetworkConnectivityObserver.kt
@@ -0,0 +1,9 @@
+package com.example.imagevista.domain.repository
+
+import com.example.imagevista.domain.model.NetworkStatus
+import kotlinx.coroutines.flow.StateFlow
+
+interface NetworkConnectivityObserver {
+
+    val networkStatus : StateFlow<NetworkStatus>
+}
